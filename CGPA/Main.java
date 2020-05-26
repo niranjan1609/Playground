de95c4541a790902abbf279ab69d2a5aa1b8f0b1
Niranjan <39219313+niranjan1609@users.noreply.github.com>
2020-05-26 11:06:32 +0530

    Test code pushed to repository.

---
+++ CGPA/Main.java
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+float CGPA;
+  cin>>CGPA;
+  cout<<CGPA;
+}
---
+++ Its result time/Main.java
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+int id,marks;
+  cin>>id>>marks;
+  cout<<"Id : "<<id<<endl<<"Totalmarks : "<<marks;
+
+}
